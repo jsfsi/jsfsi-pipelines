15e98b5a5b03a5b8795dc5f54e8274980d87fe0a
Joao Franco <joao.franco@focus-bc.com>
2019-11-12 13:20:27 +0000

    Added secrets to jenkins deployment

---
+++ jenkins/master/README.md
@@ -0,0 +1,25 @@
+# JENKINS MASTER
+
+Jenkins provides hundreds of plugins to support building, deploying and automating any project. <https://jenkins.io/>
+
+## Build
+
+```sh
+latest_version=$(curl -s http://mirrors.jenkins.io/war/ | grep latest -B 1 | head -n 1 | grep -oh -e "\d*\d\.\d\d*" | head -n 1)
+pipelines/scripts/docker/01-build.sh jenkins_master ${latest_version} ${container_registry} $(pwd)/jenkins/master/DockerFile $(pwd)/jenkins/master
+```
+
+### Publish to gcloud
+
+```sh
+gcloud auth login
+pipelines/scripts/gcloud/docker/01-publish.sh jenkins_master ${latest_version} ${container_registry}
+```
+
+### Deploy to kubernetes
+
+```sh
+gcloud auth login
+gcloud container clusters get-credentials --project="${project_id}" --zone="${cluster_zone}" "${cluster_name}"
+GOOGLE_APP_SECRET="" pipelines/scripts/kubernetes/01-deploy.sh ${application_environment_variables_file_path} ${deployment_environment_variables_file_path)
+```
---
+++ pipelines/scripts/kubernetes/01-deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -euo pipefail
+
+application_environment_variables_file_path=${1}
+deployment_environment_variables_file_path=${2}
+
+script_dir=$(dirname "$(pwd)/$0")
+
+# shellcheck disable=SC2164
+pushd "$script_dir" > /dev/null
+
+if [ -f "${application_environment_variables_file_path}" ]; then
+    # shellcheck disable=SC1090
+    source "${application_environment_variables_file_path}"
+fi
+
+# shellcheck disable=SC1090
+source "${deployment_environment_variables_file_path}"
+
+if ! kubectl get namespaces | grep "${NAMESPACE}"
+then
+    kubectl create namespace "${NAMESPACE}"
+fi
+
+temp_folder="./kubernetes-temp"
+mkdir -p "${temp_folder}/deploy"
+cp "${K8S_TEMPLATES_FOLDER}/*.yaml" "${temp_folder}"
+
+check_deployment=0
+check_daemonset=0
+
+for filename in "${temp_folder}"/*.yaml; do
+    base_filename=$(basename "$filename")
+
+    if [[ "${base_filename}" == *"configmap.yaml" ]]; then
+        # shellcheck disable=SC2086
+        # shellcheck disable=SC2016
+        envsubst '$NAMESPACE $SERVER_PORT $DOMAIN' < "$filename" > "${temp_folder}/deploy/${base_filename}"
+    else
+        # shellcheck disable=SC2086
+        envsubst < "$filename" > "${temp_folder}/deploy/${base_filename}"
+    fi
+
+    [[ $check_deployment -ne 1 ]] &&
+    [[ "${base_filename}" == *"deployment.yaml" ]] && check_deployment=1
+
+    [[ $check_daemonset -ne 1 ]] &&
+    [[ "${base_filename}" == *"daemonset.yaml" ]] && check_daemonset=1
+
+    kubectl apply -f "${temp_folder}/deploy/${base_filename}"
+done
+
+if [[ $check_deployment -eq 1 ]]; then
+    kubectl rollout status deployment "${APP_NAME}" --namespace "${NAMESPACE}"
+fi
+
+if [[ $check_daemonset -eq 1 ]]; then
+    kubectl rollout status daemonset "${APP_NAME}" --namespace "${NAMESPACE}"
+fi
+
+rm -rf "${temp_folder}"
+
+# shellcheck disable=SC2164
+popd > /dev/null
