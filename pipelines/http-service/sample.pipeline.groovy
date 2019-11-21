c4c747a5df259574aad8e763a1724a1527b52686
Joao Franco <joao.franco@focus-bc.com>
2019-11-21 17:21:11 +0000

    Clean pipeline groovy for http service

--- pipelines/http-service/sample.pipeline.groovy
+++ pipelines/http-service/sample.pipeline.groovy
@@ -7,7 +7,7 @@ pipeline {
     environment {
         APP_NAME = 'example'
         SCRIPTS_FOLDER = "pipeline/jsfsi-pipelines/pipelines/scripts"
-        ENVIRONMENTS_FOLDER = "pipeline/environments"        
+        ENVIRONMENTS_FOLDER = "$WORKSPACE/pipeline/environments"        
         VERSION = "0.0.${BUILD_NUMBER}"
         PROJECT_ID = "google cloud project id"
         CONTAINER_REGISTRY = "gcr.io/${PROJECT_ID}"        
@@ -18,33 +18,35 @@ pipeline {
         stage('Setup') {
             steps {
                 withCredentials([
-                    file(credentialsId: "${APP_NAME}-service-account-key", variable: "FILE")
+                    file(credentialsId: "example-service-account-key", variable: "SERVICE_ACCOUNT_FILE"),
+                    file(credentialsId: "jsfsi-pipelines-github-access-key", variable: "GITHUB_SSH_KEY")
                 ]) {
                     sh script: "git clean -xfd", label: "Cleanup"
-                    sh script: "gcloud auth activate-service-account --key-file=\"${FILE}\"", label: "GCloud login"                    
+                    sh script: "ssh-agent bash -c 'ssh-add $GITHUB_SSH_KEY; git submodule update --init --recursive'", label: "Init git submodules"
+                    sh script: "gcloud auth activate-service-account --key-file=\"${SERVICE_ACCOUNT_FILE}\"", label: "GCloud login"                    
                 }
             }
         }
         stage('Build') {
             steps {
-                sh script: "${SCRIPTS_FOLDER}/docker/01-build.sh ${APP_NAME} ${VERSION} ${CONTAINER_REGISTRY} packages/${APP_NAME}/Dockerfile $(pwd)", label: "Build ${APP_NAME}"
+                sh script: "$SCRIPTS_FOLDER/docker/01-build.sh $APP_NAME $VERSION $CONTAINER_REGISTRY $WORKSPACE/packages/${APP_NAME}/Dockerfile $WORKSPACE", label: "Build example"
             }
         }
         stage('Publish artifacts') {
             steps {
-                sh script: "${SCRIPTS_FOLDER}/gcloud/docker/01-publish.sh ${APP_NAME} ${VERSION} ${CONTAINER_REGISTRY} ${PROJECT_ID}", label: "Publish ${APP_NAME}"
+                sh script: "${SCRIPTS_FOLDER}/gcloud/docker/01-publish.sh ${APP_NAME} ${VERSION} ${PROJECT_ID}", label: "Publish example"
             }
         }
         stage('Deploy QA') {
             steps {
-                sh script: ". ${ENVIRONMENTS_FOLDER}/deploy.qa.env && gcloud container clusters get-credentials --project=\"${PROJECT_ID}\" --zone=\"${CLUSTER_ZONE}\" \"${CLUSTER_NAME}\"", label: "Get GKE Credentials"
-                sh script: "${SCRIPTS_FOLDER}/gcloud/kubernetes/01-deploy.sh ${ENVIRONMENTS_FOLDER}/application.qa.env ${ENVIRONMENTS_FOLDER}/deploy.qa.env" , label: "Deploy ${APP_NAME}"
+                sh script: "gcloud container clusters get-credentials --project=\"${PROJECT_ID}\" --zone=\"${CLUSTER_ZONE}\" \"${CLUSTER_NAME}\"", label: "Get GKE Credentials"
+                sh script: "${SCRIPTS_FOLDER}/gcloud/kubernetes/01-deploy.sh ${ENVIRONMENTS_FOLDER}/application.qa.env ${ENVIRONMENTS_FOLDER}/deploy.qa.env" , label: "Deploy example"
             }
         }
         stage('Deploy Production') {
             steps {
-                sh script: ". ${ENVIRONMENTS_FOLDER}/deploy.production.env && gcloud container clusters get-credentials --project=\"${PROJECT_ID}\" --zone=\"${CLUSTER_ZONE}\" \"${CLUSTER_NAME}\"", label: "Get GKE Credentials"
-                sh script: "${SCRIPTS_FOLDER}/kubernetes/01-deploy.sh ${ENVIRONMENTS_FOLDER}/application.production.env ${ENVIRONMENTS_FOLDER}/deploy.production.env", label: "Deploy ${APP_NAME}"
+                sh script: "gcloud container clusters get-credentials --project=\"${PROJECT_ID}\" --zone=\"${CLUSTER_ZONE}\" \"${CLUSTER_NAME}\"", label: "Get GKE Credentials"
+                sh script: "${SCRIPTS_FOLDER}/kubernetes/01-deploy.sh ${ENVIRONMENTS_FOLDER}/application.production.env ${ENVIRONMENTS_FOLDER}/deploy.production.env", label: "Deploy example"
             }
         }
     }
